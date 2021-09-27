c6969893220900a52cbf15f7b38ed7c5418cc105
Joel Azemar <joel.azemar@gmail.com>
2021-09-27 12:55:20 +0200

    Move logger in a higer scope

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,26 @@ module BundleLocalCacheTravisBetaTest
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # Use the lowest log level to ensure availability of diagnostic information
+    # when problems arise.
+    config.log_level = :debug
+
+    # Prepend all log lines with the following tags.
+    config.log_tags = [ :request_id ]
+    
+    # Use default logging formatter so that PID and timestamp are not suppressed.
+    config.log_formatter = ::Logger::Formatter.new
+
+    # Use a different logger for distributed setups.
+    # require 'syslog/logger'
+    # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    end
+    
   end
 end
