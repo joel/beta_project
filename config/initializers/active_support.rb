74aed7b3b7c0b5ce2ae4c1cd50d37d527bd983c0
Joel Azemar <joel.azemar@gmail.com>
2021-12-07 09:29:11 +0100

    Update active_support.rb

--- config/initializers/active_support.rb
+++ config/initializers/active_support.rb
@@ -1,10 +1,11 @@
+require "active_support"
+
 ActiveSupport::Dependencies.warnings_on_first_load = true
 
 ActiveSupport::Dependencies.singleton_class.prepend(Module.new do
   def load_missing_constant(*args)
 
     Rails.logger.debug "#{__method__}(#{args.map(&:inspect).join(', ')})"
-    puts "#{__method__}(#{args.map(&:inspect).join(', ')})"
 
     super
   end
