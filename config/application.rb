a7e79e2d5ce5ea3f727b635cc434f819634568c9
Joel Azemar <joel.azemar@gmail.com>
2021-12-07 09:29:14 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,10 @@ module BundleLocalCacheTravisBetaTest
     # require 'syslog/logger'
     # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    end
   end
 end
