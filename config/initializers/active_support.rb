ca6ccb1875512e2d18de6b5a9dc47db7523ac5c7
Joel Azemar <joel.azemar@gmail.com>
2021-12-07 08:52:53 +0100

    Create active_support.rb

---
+++ config/initializers/active_support.rb
@@ -0,0 +1,11 @@
+ActiveSupport::Dependencies.warnings_on_first_load = true
+
+ActiveSupport::Dependencies.singleton_class.prepend(Module.new do
+  def load_missing_constant(*args)
+
+    Rails.logger.debug "#{__method__}(#{args.map(&:inspect).join(', ')})"
+    puts "#{__method__}(#{args.map(&:inspect).join(', ')})"
+
+    super
+  end
+end)
