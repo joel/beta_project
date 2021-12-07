7d1288792821c8aa503b76958220ace0b1d0a1ea
Joel Azemar <joel.azemar@gmail.com>
2021-12-07 08:52:56 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,19 +10,20 @@ module BundleLocalCacheTravisBetaTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.autoloader = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
+
     # Use the lowest log level to ensure availability of diagnostic information
     # when problems arise.
     config.log_level = :debug
 
     # Prepend all log lines with the following tags.
     config.log_tags = [ :request_id ]
-    
+
     # Use default logging formatter so that PID and timestamp are not suppressed.
     config.log_formatter = ::Logger::Formatter.new
 
@@ -30,11 +31,8 @@ module BundleLocalCacheTravisBetaTest
     # require 'syslog/logger'
     # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-    if ENV["RAILS_LOG_TO_STDOUT"].present?
-      logger           = ActiveSupport::Logger.new(STDOUT)
-      logger.formatter = config.log_formatter
-      config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    end
-    
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 end
