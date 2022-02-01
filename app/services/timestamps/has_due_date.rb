0da78e263f97a814f9259bbfc666784ffb7117ff
Joel Azemar <joel.azemar@gmail.com>
2022-02-01 18:51:06 +0100

    Update has_due_date.rb

--- app/services/timestamps/has_due_date.rb
+++ app/services/timestamps/has_due_date.rb
@@ -3,26 +3,39 @@
 module Timestamps
   class HasDueDate
 
-    attr_reader :errors
+    attr_reader :errors, :deadline
 
     def initialize(input)
       if input.valid?
-        deadline_str = "#{input.date_attr} #{input.time_attr}"
+        unless input.time_attr
+          deadline_str = "#{input.date_attr}"
+        else
+          deadline_str = "#{input.date_attr} #{input.time_attr}"
+        end
+
         @deadline = Time.zone.parse(deadline_str) # Returns the TimeWithZone value
+
+        @deadline = @deadline.change(hour: 23, min: 59, sec: 59) unless input.time_attr
       else
         @errors = input.errors
       end
     end
 
     def due_date
+      return unless deadline
+
       deadline.strftime("%d/%m/%Y")
     end
 
     def due_time
+      return unless deadline
+
       deadline.strftime("%H:%M")
     end
 
     def all_day
+      return unless deadline
+
       deadline.strftime("%H:%M") == "23:59"
     end
     alias all_day? all_day
@@ -31,8 +44,5 @@ module Timestamps
       !@errors.present?
     end
 
-    private
-
-    attr_reader :deadline
   end
 end
