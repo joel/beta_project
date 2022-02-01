e605582bafadd7283125dc66148a160f4db2c0c3
Joel Azemar <joel.azemar@gmail.com>
2022-02-01 13:34:29 +0100

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'structured_warnings/test'
+
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
+  include StructuredWarnings::Test::Assertions
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
