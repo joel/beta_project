dca2a40436975441c2b5281811cdf40a6fa48a9f
Joel Azemar <joel.azemar@gmail.com>
2022-02-01 18:50:52 +0100

    Update has_due_date_test.rb

--- test/services/timestamps/has_due_date_test.rb
+++ test/services/timestamps/has_due_date_test.rb
@@ -9,11 +9,23 @@ module Timestamps
 
       should "not allow due_time to be set and due_date to be nil when created" do
         input = HasDueDateInput.new(date_attr: "31/12/2029", time_attr: "22:00", switch_attr: nil)
-        item = HasDueDate.new(input)
+        item  = HasDueDate.new(input)
 
         assert_equal "31/12/2029", item.due_date
         assert_equal "22:00", item.due_time
         assert_not item.all_day
+      end
+    end
+
+    context "due_date without time" do
+
+      should "set the due_time to the end of the day" do
+        input = HasDueDateInput.new(date_attr: "31/12/2029", time_attr: nil, switch_attr: nil)
+        item  = HasDueDate.new(input)
+
+        assert_equal "31/12/2029", item.due_date
+        assert_equal "23:59", item.due_time
+        assert item.all_day
       end
     end
 
