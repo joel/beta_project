c31ee6c88cb2c18fa500f2e9b71c610264c6af44
Joel Azemar <joel.azemar@gmail.com>
2022-02-01 15:12:02 +0100

    Create has_due_date_test.rb

---
+++ test/services/timestamps/has_due_date_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Timestamps
+  class HasDueDateTest < ActiveSupport::TestCase
+
+    context "due_date and time" do
+
+      should "not allow due_time to be set and due_date to be nil when created" do
+        item = HasDueDate.new(date_attr: "31/12/2029", time_attr: "22:00", switch_attr: nil)
+
+        assert_equal "31/12/2029", item.due_date
+        assert_equal "22:00", item.due_time
+        assert_not item.all_day
+      end
+    end
+
+  end
+end
