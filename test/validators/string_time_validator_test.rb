f73f6f998a89f1f77995e417b6ac232185a84b24
Joel Azemar <joel.azemar@gmail.com>
2022-02-01 16:44:09 +0100

    Create string_time_validator_test.rb

---
+++ test/validators/string_time_validator_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class StringTimeValidatorTest < ActiveSupport::TestCase
+
+  class Validatable
+    include ActiveModel::Model
+    include ActiveModel::Validations
+
+    attr_accessor :time_attr
+
+    validates_with StringTimeValidator
+  end
+
+
+  context "validations" do
+
+    context "time_attr" do
+
+      setup do
+        @params = {
+          time_attr: "15:30"
+        }
+      end
+
+      should "be valid" do
+        assert Validatable.new(@params).valid?
+      end
+
+      should "not be valid" do
+        input = Validatable.new(@params.merge(time_attr: "8h"))
+        assert_not input.valid?
+        assert_equal({:time_attr=>["bad format [8h] should be HH:MM"]}, input.errors.messages)
+      end
+
+    end
+
+  end
+
+end
