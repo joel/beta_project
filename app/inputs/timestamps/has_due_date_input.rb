91b1651b9ff530a734adcce33bef0a33103893fb
Joel Azemar <joel.azemar@gmail.com>
2022-02-02 09:00:35 +0100

    Update has_due_date_input.rb

--- app/inputs/timestamps/has_due_date_input.rb
+++ app/inputs/timestamps/has_due_date_input.rb
@@ -6,8 +6,8 @@ module Timestamps
     attr_accessor :date_attr, :time_attr
     attr_reader :switch_attr
 
-    validates :date_attr, presence: true
     validates_with StringDateValidator
+    validates_with StringTimeValidator
 
     def switch_attr=(value)
       warn StructuredWarnings::DeprecatedMethodWarning, "Set the flag all_day is deprecated, set [time_attr] to nil instead" unless value.blank?
