f46768d01e72551679c347c84ba77274c4c8a331
Joel Azemar <joel.azemar@gmail.com>
2022-02-02 14:04:01 +0100

    Update has_due_date_test.rb

--- test/services/timestamps/has_due_date_test.rb
+++ test/services/timestamps/has_due_date_test.rb
@@ -9,11 +9,12 @@ module Timestamps
 
       should "not allow due_time to be set and due_date to be nil when created" do
         input = HasDueDateInput.new(date_attr: "31/12/2029", time_attr: "22:00", switch_attr: nil)
-        item  = HasDueDate.new(input)
+        item  = HasDueDate.new(input: input).perform
 
-        assert_equal "31/12/2029", item.due_date
-        assert_equal "22:00", item.due_time
-        assert_not item.all_day
+        assert_nil item.errors
+
+        assert_equal "31/12/2029", item.deadline.strftime("%d/%m/%Y")
+        assert_equal "22:00", item.deadline.strftime("%H:%M")
       end
     end
 
@@ -21,11 +22,12 @@ module Timestamps
 
       should "set the due_time to the end of the day" do
         input = HasDueDateInput.new(date_attr: "31/12/2029", time_attr: nil, switch_attr: nil)
-        item  = HasDueDate.new(input)
+        item  = HasDueDate.new(input: input).perform
 
-        assert_equal "31/12/2029", item.due_date
-        assert_equal "23:59", item.due_time
-        assert item.all_day
+        assert_nil item.errors
+
+        assert_equal "31/12/2029", item.deadline.strftime("%d/%m/%Y")
+        assert_equal "23:59", item.deadline.strftime("%H:%M")
       end
     end
 
