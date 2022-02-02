b85fc91c325a470057953786fdeb9ea777bf3357
Joel Azemar <joel.azemar@gmail.com>
2022-02-02 14:03:41 +0100

    Update has_due_date.rb

--- app/lib/ar_ext/has_due_date.rb
+++ app/lib/ar_ext/has_due_date.rb
@@ -122,7 +122,7 @@ module ArExt
           input = virtual_inputs
           return unless input.valid?
 
-          service = Timestamps::HasDueDate.new(input)
+          service = Timestamps::HasDueDate.new(input: input).perform
           return unless service.deadline # Do Not Override if Set
 
           write_attribute(:deadline, service.deadline)
